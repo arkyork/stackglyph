ed1a659234067437234f5d0ef086144adfa1999b
arkyork <abcvfga123123@gmail.com>
2025-06-02 12:06:26 +0900

    note

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="icon" type="image/png" href="/icon.png">
         <title>@yield("title")</title>
-
+        <meta name="google-site-verification" content="CM0xkCc0cnHiLI9MMTAxSBH83Hk-DPcnfnRY6AahGrw" />
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @yield("head")
     </head>
